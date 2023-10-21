665b3c15c5b2ed15b42964f2363817f00b9206f4
henrry <henrrybrgt@gmail.com>
2023-10-21 19:47:40 -0400

    adding info

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,17 @@ import { Textarea } from "./components/ui/textarea";
 import { Label } from "./components/ui/label";
 import { ToGomp } from "@/lib/wailsjs/go/main/App";
 import React from "react";
+import { Info } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./components/ui/alert-dialog";
 
 function App() {
   const [gompCode, setGompCode] = React.useState("");
@@ -17,6 +28,43 @@ function App() {
 
   return (
     <div className="min-h-screen bg-white text-blue-900 text-2xl font-bold flex flex-col items-center mx-auto py-8">
+      <div className="absolute top-5 right-5">
+        <AlertDialog>
+          <AlertDialogTrigger>
+            <Button size={"icon"} variant={"default"}>
+              <Info className="h-5 w-5" />
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle className="text-2xl mb-3">
+                How To Use Gomponerter?
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                <p>
+                Copy any HTML Element with children nodes and paste it into the
+                left textarea and click on <span className="italic">Send HTML</span> button to convert it
+                into a{" "}
+                <a
+                  href="https://gomponents.com"
+                  className="underline text-blue-900 font-semibold">
+                  gomponent code
+                </a>
+                </p>
+                <p className="my-2">
+                  <span className="text-destructive">NOTE:</span> if the HTML provided has many root elements, only the first would be considered and the rest ignored
+                </p>
+              </AlertDialogDescription>
+              <AlertDialogDescription>
+                Made with love by hbourgeot using Wails
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </div>
       <h1>
         Gomponerter: Convert HTML code to{" "}
         <a href="https://gomponents.com" className="underline">
@@ -26,8 +74,8 @@ function App() {
       <p className="text-lg text-black">
         Write here your HTML Code like you do normally
       </p>
-      <div className="flex justify-evenly items-center space-x-4 p-5 w-[90%] gap-5 flex-wrap">
-        <div className="flex flex-col items-center gap-y-4 w-[40%]">
+      <div className="flex justify-evenly items-center space-x-4 p-5 w-[90%] gap-1 flex-wrap">
+        <div className="flex flex-col items-center gap-y-4 w-[47%]">
           <Label htmlFor="html" className="self-start">
             HTML code
           </Label>
@@ -41,7 +89,7 @@ function App() {
           />
         </div>
         <div className="border-l-2 border-l-gray-300/50 border-solid hidden md:block w-1/10 h-[100px]"></div>
-        <div className="flex flex-col items-center w-[40%] gap-y-4 !mt-0">
+        <div className="flex flex-col items-center w-[47%] gap-y-4 !mt-0">
           <Label htmlFor="gomp" className="self-start">
             Gomponent code
           </Label>
